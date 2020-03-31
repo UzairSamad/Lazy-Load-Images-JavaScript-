f0dd0576706147ca1c846175c286fac3af791716
M.Uzair <uzair.iu17@gmail.com>
2020-03-31 20:49:38 +0500

    added images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import lazy from './image/lazy_img.jpg'
+import image1 from './image/img_1.jpg'
+import image2 from './image/img_2.jpg'
 
 function App() {
   return (
     <div className="App">
  
  <h1>Lazy Loading images...</h1>
-  <img src="lazy_img.jpg" data-src="img_1.jpg" class="lazy" />
-  <img src="lazy_img.jpg" data-src="img_2.jpg" class="lazy" />
+  <img src={lazy} data-src={image1} class="lazy" />
+  <img src={lazy} data-src={image2} class="lazy" />
 
   
     </div>
